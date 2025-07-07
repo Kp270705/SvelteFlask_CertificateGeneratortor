61771900b3413da183addcc659f80f660e98b3cc
Kunal Pathak <112516865+Kp270705@users.noreply.github.com>
2025-07-07 22:15:29 +0530

    Update fileHandle.py

--- backend/fileHandle.py
+++ backend/fileHandle.py
@@ -1,16 +1,1 @@
-import os
-import shutil
 
-
-# How to read, all file names of a folder:
-def readFileNames(folderPatha):
-    # Get all files and directories
-    all_entries = os.listdir(folderPath)
-    # Filter only files
-    file_names = [f for f in all_entries if os.path.isfile(os.path.join(folderPath, f))]
-    print(file_names)
-
-
-if __name__ == "__main__":
-    folderPath = './fonts'
-    filesList = readFileNames(folderPath)
